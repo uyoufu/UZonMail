9790e5dadf6b10169fd8a42c9d7aa18d41be55d8
galens <gmx_galens@163.com>
2025-01-08 13:33:15 +0800

    perf: 添加章节序号

--- ui-src/src/router/types.ts
+++ ui-src/src/router/types.ts
@@ -6,7 +6,7 @@ export type ExtendedRouteRecordRaw = RouteRecordRaw & {
     label: string, // 名称
     icon: string, // 图标
     access?: string[], // 访问权限，当有该权限时，才显示，权限是且的关系，即同时具有多个权限才显示
-    denies?: string[], // 当有该权限时，拒绝显示，权限是或的关系，即只要有一个权限就不显示
+    denies?: string[], // 当有该权限时，拒绝显示，权限是或的关系，即只要有一个权限就不显示, 该值优先于 access
     noTag?: boolean, // 是否不显示标签
     noMenu?: boolean // 是否不显示菜单
   }
