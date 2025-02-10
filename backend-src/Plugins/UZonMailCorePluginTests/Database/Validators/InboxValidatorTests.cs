850dd26b6b93adc27185ee902118199e2391e306
galens.gmx <260827400@qq.com>
2025-02-10 18:01:39 +0800

    perf: 添加单元测试

---
+++ backend-src/Plugins/UZonMailCorePluginTests/Database/Validators/InboxValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UZonMail.Core.Database.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UZonMail.DB.SQL.Emails;
+
+namespace UZonMail.Core.Database.Validators.Tests
+{
+    [TestClass()]
+    public class InboxValidatorTests
+    {
+        [DataRow("linzirui@ra.sc.e.titech.ac.jp")]
+        [TestMethod()]
+        public void InboxValidatorTest(string email)
+        {
+            var inbox = new Inbox()
+            {
+                Email = email
+            };
+            var validator = new InboxValidator();
+            var vdResult = validator.Validate(inbox);
+            Assert.IsTrue(vdResult.IsValid);
+        }
+    }
+}
