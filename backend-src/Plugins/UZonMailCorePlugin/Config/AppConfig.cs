e3d5a68181b07205f50cc1cf174f94b0098b1a01
galens.gmx <260827400@qq.com>
2025-05-26 12:53:32 +0800

    feat: 更新使用说明

--- backend-src/Plugins/UZonMailCorePlugin/Config/AppConfig.cs
+++ backend-src/Plugins/UZonMailCorePlugin/Config/AppConfig.cs
@@ -11,7 +11,6 @@ namespace UZonMail.Core.Config
         public DatabaseConfig DataBase { get; set; }
         public HttpConfig Http { get; set; }
         public LoggerConfig Logger { get; set; }
-        public SystemConfig System { get; set; }
         public UserConfig User { get; set; }
         public WebsocketConfig Websocket { get; set; }
         public TokenParams TokenParams { get; set; }
