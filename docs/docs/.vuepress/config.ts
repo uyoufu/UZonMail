513871fa458f14aeef0d9b8647bfceb270a4e2df
galens.gmx <260827400@qq.com>
2025-07-08 08:46:41 +0800

    feat: 添加百度站长验证

--- docs/docs/.vuepress/config.ts
+++ docs/docs/.vuepress/config.ts
@@ -11,7 +11,19 @@ export default defineUserConfig({
     '宇正群邮是一款开源的邮件群发软件，提供邮件群发、邮件营销、邮箱爬取、任意变量等诸多功能。支持所有类型邮箱账号，包括Outlook的OAuth2。原生企业级品质，支持多端用户，支持Windows、Linux、MacOS等操作系统, 支持服务器部署。原生多线程并发，支持多账号同时使用，性能强劲。多年持续迭代更新优化，已被外贸营销、教育培训、财务会计等多个行业广泛使用。',
   theme,
   head: [
-    ['title', {}, '宇正群邮 - 邮件群发|邮件群发软件|邮件营销软件|开源邮件群发|最好用的邮件群发软件']
+    [
+      'title',
+      {},
+      '宇正群邮 - 邮件群发|邮件群发软件|邮件营销软件|开源邮件群发|最好用的邮件群发软件'
+    ],
+    // 百度验证
+    [
+      'meta',
+      {
+        name: 'baidu-site-verification',
+        content: 'codeva-TLGtlr39fQ'
+      }
+    ]
   ]
   // 和 PWA 一起启用
   // shouldPrefetch: false,
