105bc1da38f98b7550a9258e8b35970124242c74
galens.gmx <260827400@qq.com>
2025-08-25 18:55:29 +0800

    fix: 修复js变量无法解析的bug

--- backend-src/Plugins/UZonMailCorePlugin/Services/EmailDecorator/EmailVariableHelper.cs
+++ backend-src/Plugins/UZonMailCorePlugin/Services/EmailDecorator/EmailVariableHelper.cs
@@ -13,11 +13,26 @@ namespace UZonMail.Core.Services.EmailDecorator
         public static List<string> GetAllVariableNames(string content)
         {
             // 获取内容中的变量
-            var matches = Regex.Matches(content, @"\{\{\s*([\p{L}\p{N}_]+)\s*\}\}",RegexOptions.None);
+            var matches = Regex.Matches(content, @"\{\{\s*([\p{L}\p{N}_\.]+)\s*\}\}", RegexOptions.None);
             var variables = matches.Select(m => m.Groups[1].Value)
                 .Distinct()
                 .ToList();
             return variables;
+        }
+
+        /// <summary>
+        /// 获取内容中的所有变量内容
+        /// 变量内容格式为: {{内容}}
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static List<string> GetAllVariableContents(string content)
+        {
+            var matches = Regex.Matches(content, @"\{\{\s*((.*?))\s*\}\}", RegexOptions.Multiline);
+            var contents = matches.Select(m => m.Groups[1].Value)
+                .Distinct()
+                .ToList();
+            return contents;
         }
     }
 }
