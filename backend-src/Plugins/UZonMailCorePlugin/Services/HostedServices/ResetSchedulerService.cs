174dd8dd1469ec7a4183ad68700a968514cff6b6
galens.gmx <260827400@qq.com>
2025-10-13 12:27:24 +0800

    perf: 保存文件变动

--- backend-src/Plugins/UZonMailCorePlugin/Services/HostedServices/ResetSchedulerService.cs
+++ backend-src/Plugins/UZonMailCorePlugin/Services/HostedServices/ResetSchedulerService.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Quartz;
+﻿using Quartz;
 using UZonMail.Core.Jobs;
-using UZonMail.Utils.Web.Service;
 
 namespace UZonMail.Core.Services.HostedServices
 {
@@ -18,7 +16,7 @@ namespace UZonMail.Core.Services.HostedServices
 
             #region 重置每日发件限制
             var jobKey = new JobKey($"schduleTask-resetSentCountToday");
-            bool exist = await scheduler.CheckExists(jobKey);
+            bool exist = await scheduler.CheckExists(jobKey, stoppingToken);
             if (exist) return;
 
             var job = JobBuilder.Create<SentCountReseter>()
