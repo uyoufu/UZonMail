6be7d9a592ace1b43fe248855a310bbf3ab5e28c
galens.gmx <260827400@qq.com>
2025-10-20 13:14:26 +0800

    perf: 后端的 BaseUrl 配置强制覆盖前端配置

--- backend-src/Plugins/UZonMailCorePlugin/Services/HostedServices/SetupBaseUrlWhenStartp.cs
+++ backend-src/Plugins/UZonMailCorePlugin/Services/HostedServices/SetupBaseUrlWhenStartp.cs
@@ -20,11 +20,7 @@ namespace UZonMail.Core.Services.HostedServices
             if(string.IsNullOrEmpty(baseUrl)) return Task.CompletedTask;
 
             // 读取为 json
-            var jobject = JObject.Parse(File.ReadAllText(frontConfigPath));
-            var oldBaseUrl = jobject["baseUrl"]?.ToString();
-            // 说明已经被用户修改过了
-            if (oldBaseUrl == null || !oldBaseUrl.Contains("localhost")) return Task.CompletedTask;
-
+            var jobject = JObject.Parse(File.ReadAllText(frontConfigPath));            
             jobject["baseUrl"] = baseUrl;
             // 保存文件
             File.WriteAllText(frontConfigPath, jobject.ToString());
